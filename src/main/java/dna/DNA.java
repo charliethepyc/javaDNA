7a931badd01b8e1e24e4ea97106107a6ce3ce6b1
charliethepyc <charliechan6667@gmail.com>
2022-12-23 17:43:16 -0800

    start project

--- src/main/java/dna/DNA.java
+++ src/main/java/dna/DNA.java
@@ -1,2 +1,10 @@
+package dna;
+public class DNA{
+    private double mass;
+
+    public DNA(String sequence){
+        
+    }
+}
 // TODO: Implement the DNA datatype from scratch!
 // Use the test cases to guide you.
